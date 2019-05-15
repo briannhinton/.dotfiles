a3d2731bf2409033d5e8af368413cec6baf63697
Brian Hinton <briannhinton@gmail.com>
2019-05-15 12:17:19 -0400

    Update .hyper.js

--- .hyper.js
+++ .hyper.js
@@ -1,34 +1,41 @@
 // Future versions of Hyper may add additional config options,
 // which will not automatically be merged into this file.
 // See https://hyper.is#cfg for all currently supported options.
+
 module.exports = {
   config: {
-// Choose either "stable" for receiving highly polished,
-// or "canary" for less polished but more frequent updates
+    // Choose either "stable" for receiving highly polished,
+    // or "canary" for less polished but more frequent updates
     updateChannel: 'stable',
-// default font size in pixels for all tabs
+
+    // default font size in pixels for all tabs
     fontSize: 16,
-// font family with optional fallbacks
-    fontFamily: '"Sauce Code Nerd Font", monospace',
-// text color
+    // font family with optional fallbacks
+    fontFamily: '"MesloLGMDZ Nerd Font", monospace',
+
+    // text color
     foregroundColor: '#fff', // currently overwritten by plugin
     backgroundColor: '#000', // currently overwritten by plugin
     borderColor: '#333', // currently overwritten by plugin
-// cursor background color and opacity (hex, rgb, hsl, hsv, hwb or cmyk)
-// cursorColor: 'rgba(248,28,229,0.8)',
-// `BEAM` for |, `UNDERLINE` for _, `BLOCK` for █
+
+    // cursor background color and opacity (hex, rgb, hsl, hsv, hwb or cmyk)
+    // cursorColor: 'rgba(248,28,229,0.8)',
+    // `BEAM` for |, `UNDERLINE` for _, `BLOCK` for █
     cursorShape: 'BEAM',
     cursorBlink: true,
-// custom css to embed in the main window
+
+    // custom css to embed in the main window
     css: '',
-// custom css to embed in the terminal window
+    // custom css to embed in the terminal window
     termCSS: '',
-// custom padding (css format, i.e.: `top right bottom left`)
-    padding: '16px 16px',
-// the full list. if you're going to provide the full color palette,
-// including the 6 x 6 color cubes and the grayscale map, just provide
-// an array here instead of a color map object
-// Note: all these colors are currently overwritten by a theme plugin
+
+    // custom padding (css format, i.e.: `top right bottom left`)
+    padding: '12px 14px',
+
+    // the full list. if you're going to provide the full color palette,
+    // including the 6 x 6 color cubes and the grayscale map, just provide
+    // an array here instead of a color map object
+    // Note: all these colors are currently overwritten by a theme plugin
     colors: {
       black: '#000000',
       red: '#ff0000',
@@ -47,27 +54,35 @@ module.exports = {
       lightCyan: '#00C7FF',
       lightWhite: '#ffffff'
     },
-// the shell to run when spawning a new session (i.e. /usr/local/bin/fish)
-// if left empty, your system's login shell will be used by default
+    
+    // the shell to run when spawning a new session (i.e. /usr/local/bin/fish)
+    // if left empty, your system's login shell will be used by default
     shell: '/bin/zsh',
-// for setting shell arguments (i.e. for using interactive shellArgs: ['-i'])
-// by default ['--login'] will be used
+
+    // for setting shell arguments (i.e. for using interactive shellArgs: ['-i'])
+    // by default ['--login'] will be used
     shellArgs: ['--login'],
-// An object of environment variables to set before launching shell
+
+    // An object of environment variables to set before launching shell
     env: {},
-// if true, selected text will automatically be copied to the clipboard
+
+    // if true, selected text will automatically be copied to the clipboard
     copyOnSelect: false,
-// if true, on right click selected text will be copied or pasted if no
-// selection is present (true by default on Windows)
+
+    // if true, on right click selected text will be copied or pasted if no
+    // selection is present (true by default on Windows)
     quickEdit: false,
-// The number of rows to be persisted in terminal buffer for scrolling
+
+    // The number of rows to be persisted in terminal buffer for scrolling
     scrollback: 10000,
-// hyperborder config
+
+    // hyperborder config
     hyperBorder: {
       borderColors: ['#1D976C', '#93F9B9'],
       blurredColors: ['#177C59', '#84E0A6']
     },
-// hyper-pane config
+
+    // hyper-pane config
     paneNavigation: {
       debug: false,
       hotkeys: {
@@ -92,27 +107,32 @@ module.exports = {
       focusOnMouseHover: false,
       inactivePaneOpacity: 0.6 // Set to 1 to disable inactive panes dimming
     },
-// set to `true` (without backticks) if you're using a Linux setup that doesn't show native menus
-// default: `false` on Linux, `true` on Windows (ignored on macOS)
+
+      // set to `true` (without backticks) if you're using a Linux setup that doesn't show native menus
+    // default: `false` on Linux, `true` on Windows (ignored on macOS)
     showHamburgerMenu: '',
-// set to `false` if you want to hide the minimize, maximize and close buttons
-// additionally, set to `'left'` if you want them on the left, like in Ubuntu
-// default: `true` on windows and Linux (ignored on macOS)
+
+    // set to `false` if you want to hide the minimize, maximize and close buttons
+    // additionally, set to `'left'` if you want them on the left, like in Ubuntu
+    // default: `true` on windows and Linux (ignored on macOS)
     showWindowControls: '',
   },
-// a list of plugins to fetch and install from npm
-// format: [@org/]project[#version]
-// examples:
-//   `hyperpower`
-//   `@company/project`
-//   `project#1.0.1`
+
+  // a list of plugins to fetch and install from npm
+  // format: [@org/]project[#version]
+  // examples:
+  //   `hyperpower`
+  //   `@company/project`
+  //   `project#1.0.1`
   plugins: ["hyperminimal", "hyperborder", "hyper-night-owl", "hyper-search", "hyper-pane", "hypercwd", "hyper-tab-icons"],
-// in development, you can create a directory under
-// `~/.hyper_plugins/local/` and include it here
-// to load it and avoid it being `npm install`ed
+
+  // in development, you can create a directory under
+  // `~/.hyper_plugins/local/` and include it here
+  // to load it and avoid it being `npm install`ed
   localPlugins: [],
+
   keymaps: {
-// Example
-// 'window:devtools': 'cmd+alt+o'
+    // Example
+    // 'window:devtools': 'cmd+alt+o'
   }
 };
